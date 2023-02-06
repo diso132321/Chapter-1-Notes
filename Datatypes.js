68a80c7f7db45d8c1512d6297de52bad3edc05e0
u4h8 <u4h8@fresnoU.org>
2023-02-06 09:43:19 -0800

    Ch. 1 Notes

---
+++ Datatypes.js
@@ -0,0 +1,34 @@
+console.log(100);
+//This is a comment
+
+//Numbers
+//Integers
+console.log(-3);
+console.log(0);
+console.log(3);
+//floating point number
+console.log(3.14);
+console.log(100.045);
+
+//strings
+//Characters, letters, symbols, spaces
+console.log("Hello World");
+console.log("Hello World!!");
+console.log("Hello World 123");
+console.log('16.0');
+
+//Arithmetic Operators (math)
+console.log(14+2);
+console.log(14-2);
+console.log(14*2); //Shift-8 = *
+console.log(14/2); // % is called MOD and means remainder after dividing
+console.log(15%10);
+
+//Concatenation: combine strings
+console.log("My Name"+"Is Diego")
+console.log('My' + ' ' + 'age' + ' ' + 15 + '.');
+console.log("Your age is\n17.");// \n means new line
+
+//
+//
+console.log(12%4);
